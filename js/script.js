0a224056f227937a2fed433a4ceb2378032e42c3
MailoFenn <nikitakulkov7@gmail.com>
2019-11-26 21:07:17 +0300

    fixed

--- js/script.js
+++ js/script.js
@@ -5,21 +5,24 @@ $(function(){
         if ((head!='')||(nat!='')) {
             $('.comments_none').hide();
             var newElement = $(`
-                <div class="point">
-                    <div class="head-container">
-                        <h3 class="head">${head}</h3>
-                        <img src="img/delete.png" class="delete" alt="delete">
-                        <span class="arrow-container"><img src="img/arrow.png" class="arrow" alt="arrow"></span>
-                    </div>
-                    <p class="nat">${nat}</p>
-                </div>`);
+            <div class="point">
+            <div class="head-container">
+                <h3 class="head">${head}</h3>
+                <button class="delete"></button>
+                <button class="arrow-container arrow"></button>
+            </div>
+            <p class="nat">${nat}</p>
+        </div>`);
             $('#list').prepend(newElement);
             $('.text').val('');
+        } else {
+            $('.text').css('border-color', 'red');
         }
     });
 
     $('#list').on('click', '.delete', function(){
         $(this).parents('.point').remove();
+        console.log($('.arrow').css('background-image', '../img/arrow.png'));
 
         var items = $('.point');
         if (items.length == 1) {
@@ -28,24 +31,24 @@ $(function(){
         }
     });
 
-    $('#list').on('click','.arrow',function(){
-        if ($(this).attr('src')=='img/arrow.png') {
+    $('#list').on('click', '.arrow', function(){
+        console.log('this work!');
             $(this).parents('.point').children('.nat').animate({
                 height: 0,
                 'padding-top': 0,
                 'padding-bottom': 0
             }, 400);
             $(this).parents('.point').children('.nat').fadeOut(0);
-            $(this).attr('src', 'img/arrow-left.png');
-        } else if ($(this).attr('src')=='img/arrow-left.png') {
-            $(this).parents('.point').children('.nat').fadeIn(0);
+            $(this).css('transform', 'rotate(-90deg)');
+            
+            if($(this).parents('.point').children('.nat').css('height')=='0px'){ //условие, если высота блока с описанием равна 0px
+                $(this).parents('.point').children('.nat').fadeIn(0);
             $(this).parents('.point').children('.nat').animate({
                     height: '85px',
                     'padding-top': '20px',
                     'padding-bottom': '20px'
             }, 400);
-            $(this).attr('src', 'img/arrow.png');
-        }
-        
+            $(this).css('transform', 'rotate(0deg)');            
+            }        
     });
 });
